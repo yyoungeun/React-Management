7ef8517ae7cd2ae06ac0691eebf5e2de1fc320be
yyoungeun <1150amy@naver.com>
2020-04-01 15:06:25 +0900

    Implement Customer Add Module & Customer List Refresh Module

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import Customer from './components/Customer'
+import Customer from './components/Customer';
+import CustomerAdd from './components/CustomerAdd';
 import './App.css';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
@@ -41,9 +42,23 @@ const styles = theme => ({
 */
 class App extends Component {
 
-  state = {     //변경될 수 있는 데이터
-    customers: "",
-    completed: 0
+  constructor(props){
+    super(props);
+    this.state = {
+      customers: '',
+      completed: 0
+    }
+  }
+
+  stateRefresh = () => {
+    this.setState({
+      customers: '',
+      completed: 0
+    });
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+
   }
 
   componentDidMount() {
@@ -66,31 +81,34 @@ class App extends Component {
   render(){
     const{ classes } = this.props;
   return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell>번호</TableCell>
-            <TableCell>이미지</TableCell>
-            <TableCell>이름</TableCell>
-            <TableCell>생년월일</TableCell>
-            <TableCell>성별</TableCell>
-            <TableCell>직업</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-        {/* 고객컴포넌트 출력 부분 */}
-      {this.state.customers ? this.state.customers.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);
-       }) : 
-       <TableRow>
-         <TableCell colSpan="6" align="center">
-           <CircularProgress className={classes.progress} variant = "determinate" value={this.state.completed}/>  
-         </TableCell>
-       </TableRow>
-       }
-      </TableBody>
-      </Table>
-    </Paper>
+    <div>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {/* 고객컴포넌트 출력 부분 */}
+        {this.state.customers ? this.state.customers.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);
+        }) : 
+        <TableRow>
+          <TableCell colSpan="6" align="center">
+            <CircularProgress className={classes.progress} variant = "determinate" value={this.state.completed}/>  
+          </TableCell>
+        </TableRow>
+        }
+        </TableBody>
+        </Table>
+      </Paper>
+      <CustomerAdd stateRefresh={this.stateRefresh}/>
+    </div>
   );
   }
 }
---
+++ client/src/components/CustomerAdd.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import {post} from 'axios';
+
+class CustomerAdd extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            file: null,  //byte형태의 data
+            userName:'',
+            birthday: '',
+            gender: '',
+            job: '',
+            fileName: '' //보내고자하는 파일 이미지의 이름(파일명)
+        }
+    }
+
+    handleFormSubmit = (e) => {
+        e.preventDefault()
+        this.addCustomer()
+        .then((response) => {
+            console.log(response.data);
+            this.props.stateRefresh();
+        })
+        this.setState({
+            file: null,
+            userName: '',
+            birthday: '',
+            gender: '',
+            job: '',
+            fileName: ''
+        })
+        //window.location.reload();
+        //this.props.stateRefresh();
+    }
+
+    handleFileChange = (e) => {
+        this.setState({
+            file: e.target.files[0],
+            fileName: e.target.value
+        })
+    }
+
+    handleValueChange = (e) => {
+        let nextState = {};
+        nextState[e.target.name] = e.target.value;
+        this.setState(nextState);
+    }
+
+    addCustomer = () => {
+        const url = '/api/customers';
+        const formData = new FormData();
+        formData.append('image', this.state.file);
+        formData.append('name', this.state.userName);
+        formData.append('birthday',this.state.birthday);
+        formData.append('gender', this.state.gender);
+        formData.append('job', this.state.job);
+        const config = {
+            headers:{
+                'content-type': 'multipart/form-data' //전달하고자 하는 데이터의 파일이 포함되고 있을 때 설정
+            }
+        }
+        return post(url, formData, config);
+    }
+
+    render(){
+        return( //고객 추가 양식
+            <form onSubmit={this.handleFormSubmit}>
+                <h1>고객 추가</h1>
+                프로필 이미지: <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange} /><br/>
+                이름: <input type="text" name="userName" vlaue={this.state.userName} onChange={this.handleValueChange}/><br/>
+                생년월일: <input type="text" name="birthday" vlaue={this.state.birthday} onChange={this.handleValueChange}/><br/>
+                성별: <input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}/><br/>
+                직업: <input type="text" name="job" value={this.state.job} onChange={this.handleValueChange}/><br/>
+                <button type="submit">추가하기</button>
+            </form>
+        )
+    }
+}
+
+export default CustomerAdd;
